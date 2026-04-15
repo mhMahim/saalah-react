a3bd39f05c3fd21d375bc8c76a9adc93f5493745
mahimerror <mahimislam46@gmail.com>
2026-04-15 11:00:48 +0600

    implement hero section api

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -1,23 +1,29 @@
-import banner from "@/assets/images/hero-banner.png";
 import Container from "../shared/Container";
 import { useLanguageContext } from "@/hooks/useLanguageContext";
+import useFetchData from "@/hooks/useFetchData";
 
 const HeroSection = () => {
-  const { t } = useLanguageContext();
+  const { language } = useLanguageContext();
+  const { data } = useFetchData("/cms/home-banner");
 
   return (
     <div className="relative">
       <Container className="relative z-10 text-white pt-30 pb-20 sm:py-36 lg:py-40 xl:py-55">
-        <h1 className="text-[28px] sm:text-4xl lg:text-5xl xl:text-[64px] font-semibold leading-tight sm:max-w-[90%]">
-          {t("hero.title")}
+        <h1 className="text-[28px] sm:text-4xl lg:text-5xl xl:text-[64px] font-semibold leading-tight sm:max-w-[90%] min-h-25 xl:min-h-50">
+          {language === "en" ? data?.data?.title_en : data?.data?.title_fr}
         </h1>
       </Container>
       <figure className="absolute inset-0">
-        <img
-          src={banner}
-          alt="Hero Banner"
-          className="h-full w-full object-cover object-center"
-        />
+        {data?.data?.banner_full_url ? (
+          <img
+            src={data?.data?.banner_full_url}
+            alt="Hero Banner"
+            loading="lazy"
+            className="h-full w-full object-cover object-center"
+          />
+        ) : (
+          <div className="h-full w-full bg-gray-300 flex items-center justify-center" />
+        )}
         <div className="absolute inset-0 bg-black/50"></div>
       </figure>
     </div>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,7 @@
-import { Outlet } from "react-router";
+import { Outlet, ScrollRestoration } from "react-router";
 import Navbar from "@/components/layout/Navbar";
-import { useEffect } from "react";
 import { useLocation } from "react-router";
 import Footer from "@/components/layout/Footer";
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
 
 const MainLayout = () => {
   const { pathname } = useLocation();
@@ -22,7 +11,7 @@ const MainLayout = () => {
     <div
       className={`font-poppins min-h-screen ${isHome ? "bg-white" : "bg-[#F4F6F8]"}`}
     >
-      <ScrollToTop />
+      <ScrollRestoration />
       <Navbar />
       <main>
         <Outlet />
