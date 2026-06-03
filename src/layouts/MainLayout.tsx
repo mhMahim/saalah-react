6628e5d5bf799304316e9bd123ce837cf8ddcc6b
mahimerror <mahimislam46@gmail.com>
2026-06-03 15:03:24 +0600

    favicon set

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
-import { Outlet, ScrollRestoration } from "react-router";
+import { Outlet, ScrollRestoration, useLocation } from "react-router";
 import Navbar from "@/components/layout/Navbar";
-import { useLocation } from "react-router";
 import Footer from "@/components/layout/Footer";
 import { Helmet } from "react-helmet-async";
 import { useStateContext } from "@/hooks/useStateContext";
@@ -10,13 +9,33 @@ const MainLayout = () => {
   const isHome = pathname === "/";
   const { systemSettingsData } = useStateContext();
 
+  const isSafari =
+    typeof window !== "undefined" &&
+    /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  const favicon = isSafari
+    ? "/favicon.png" // your static favicon path
+    : systemSettingsData?.data?.favicon;
+
   return (
     <>
       <Helmet>
         <title>{systemSettingsData?.data?.title}</title>
+
+        {favicon && (
+          <link
+            rel="icon"
+            type="image/png"
+            href={isSafari ? favicon : `${favicon}?v=${Date.now()}`}
+            key={favicon}
+          />
+        )}
       </Helmet>
+
       <div
-        className={`font-poppins min-h-screen ${isHome ? "bg-white" : "bg-[#F4F6F8]"}`}
+        className={`font-poppins min-h-screen ${
+          isHome ? "bg-white" : "bg-[#F4F6F8]"
+        }`}
       >
         <ScrollRestoration />
         <Navbar />
